62f31f423508b7ade58603176f53fac1f11764c8
Carlos Rodriguez <carlos@interchain.io>
2024-04-22 19:47:22 +0200

    comment out

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,11 +1,11 @@
 use cosmwasm_schema::write_api;
 
-use ibc_client_cw::types::{InstantiateMsg, SudoMsg, QueryMsg};
+use ibc_client_cw::types::{InstantiateMsg, SudoMsg};
 
 fn main() {
     write_api! {
         instantiate: InstantiateMsg,
         sudo: SudoMsg,
-        query: QueryMsg,
+        //query: QueryMsg,
     }
 }
