5b248dce0f649d1c162e0a1a5022fdc4779a49fd
Ranadeep Biswas <mail@rnbguy.at>
2024-04-29 12:22:46 +0200

    impl TryFrom and From

--- src/consensus_state/definition.rs
+++ src/consensus_state/definition.rs
@@ -25,6 +25,22 @@ impl TryFrom<AnyConsensusState> for TendermintConsensusState {
     }
 }
 
+impl From<TendermintConsensusStateType> for AnyConsensusState {
+    fn from(value: TendermintConsensusStateType) -> Self {
+        AnyConsensusState::Rollkit(value.into())
+    }
+}
+
+impl TryFrom<AnyConsensusState> for TendermintConsensusStateType {
+    type Error = ClientError;
+
+    fn try_from(value: AnyConsensusState) -> Result<Self, Self::Error> {
+        match value {
+            AnyConsensusState::Rollkit(cs) => Ok(cs.into_inner()),
+        }
+    }
+}
+
 impl From<AnyConsensusState> for Any {
     fn from(value: AnyConsensusState) -> Self {
         match value {
